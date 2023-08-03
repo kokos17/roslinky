022265aace16e54e978077fc64ee815c033d3ef4
Artem Dadashjants <ketronix2@gmail.com>
2023-08-03 13:58:38 +0300

    Created swipe controller

---
+++ Assets/Scripts/SwipeControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SwipeControl : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    private Vector2 origin;
+    private Vector2 direction;
+
+    private void Awake()
+    {
+        direction = Vector2.zero;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        origin = eventData.position;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 CurPos = eventData.position;
+        Vector2 dirRaw = CurPos - origin;
+        direction = dirRaw.normalized;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        direction = Vector2.zero;
+    }
+
+    public Vector2 GetDir()
+    {
+        return direction;
+    }
+}
